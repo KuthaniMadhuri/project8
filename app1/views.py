a616435a611375dbdeee5f34736af72965c2702e
first project <madhurikuthani132@gmail.com>
2022-11-25 17:41:21 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def condition(request):
+    d={'a':100}
+    return render(request,'condition.html',context=d)
+def  condition(request):
+    d={'a':100,'b':200}
+    return render(request,'condition.html',context=d)
